3c2fd6aa29b469c4eb8c9974bad86cb63043f058
dogoodthings <root666@gmx.de>
2023-11-15 08:39:16 +0100

    use new KeyConverterService to work with document key fields (type/number/version/part)

--- src/main/java/org/dogoodthings/ectr/clone/Activator.java
+++ src/main/java/org/dogoodthings/ectr/clone/Activator.java
@@ -2,28 +2,30 @@ package org.dogoodthings.ectr.clone;
 
 import com.dscsag.plm.spi.interfaces.ECTRService;
 import com.dscsag.plm.spi.interfaces.process.extensions.CloneDocumentsExtensionService;
+import com.dscsag.plm.spi.interfaces.services.document.key.KeyConverterService;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 
 public class Activator implements BundleActivator {
-    public void start(BundleContext context) throws Exception {
-        // first, get ECTRService which provides some common
-        // useful services like logging, dictionary and so on.
-        ECTRService ectrService = getService(context, ECTRService.class);
+  public void start(BundleContext context) throws Exception {
+    // first, get ECTRService which provides some common
+    // useful services like logging, dictionary and so on.
+    ECTRService ectrService = getService(context, ECTRService.class);
+    KeyConverterService keyConverterService = getService(context, KeyConverterService.class);
 
-        context.registerService(CloneDocumentsExtensionService.class, new CloneDependentDocumentService(ectrService), null);
-    }
+    context.registerService(CloneDocumentsExtensionService.class, new CloneDependentDocumentService(ectrService, keyConverterService), null);
+  }
 
-    @Override
-    public void stop(BundleContext context) throws Exception {
-        // empty
-    }
+  @Override
+  public void stop(BundleContext context) throws Exception {
+    // empty
+  }
 
-    private <T> T getService(BundleContext context, Class<T> clazz) throws Exception {
-        ServiceReference<T> serviceRef = context.getServiceReference(clazz);
-        if (serviceRef != null)
-            return context.getService(serviceRef);
-        throw new Exception("Unable to find implementation for service " + clazz.getName());
-    }
+  private <T> T getService(BundleContext context, Class<T> clazz) throws Exception {
+    ServiceReference<T> serviceRef = context.getServiceReference(clazz);
+    if (serviceRef != null)
+      return context.getService(serviceRef);
+    throw new Exception("Unable to find implementation for service " + clazz.getName());
+  }
 }
--- src/main/java/org/dogoodthings/ectr/clone/CloneDependentDocumentService.java
+++ src/main/java/org/dogoodthings/ectr/clone/CloneDependentDocumentService.java
@@ -1,5 +1,6 @@
 package org.dogoodthings.ectr.clone;
 
+import com.dscsag.plm.spi.interfaces.DocumentKey;
 import com.dscsag.plm.spi.interfaces.ECTRService;
 import com.dscsag.plm.spi.interfaces.objects.PlmObjectKey;
 import com.dscsag.plm.spi.interfaces.process.PluginProcessContainer;
@@ -7,27 +8,24 @@ import com.dscsag.plm.spi.interfaces.process.extensions.CloneDocumentsExtensionS
 import com.dscsag.plm.spi.interfaces.rfc.RfcCall;
 import com.dscsag.plm.spi.interfaces.rfc.RfcResult;
 import com.dscsag.plm.spi.interfaces.rfc.RfcStructure;
+import com.dscsag.plm.spi.interfaces.services.document.key.KeyConverterService;
 import com.dscsag.plm.spi.rfc.builder2.FmCallBuilder;
-import com.google.auto.service.AutoService;
 
 import java.util.Collections;
 import java.util.Set;
 import java.util.function.Consumer;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
-@AutoService(CloneDocumentsExtensionService.class)
 public class CloneDependentDocumentService implements CloneDocumentsExtensionService {
 
-  private static final Pattern PATTERN = Pattern.compile("(...)(.{25})(.{2})(.*)");
   private static final Set<String> ERROR_RET_TYPES = Set.of("A", "X", "E");
   private final ECTRService ectrService;
+  private final KeyConverterService keyConverter;
 
-  public CloneDependentDocumentService(ECTRService ectrService) {
+  public CloneDependentDocumentService(ECTRService ectrService, KeyConverterService keyConverter) {
     this.ectrService = ectrService;
+    this.keyConverter = keyConverter;
   }
-
 
   @Override
   public Consumer<PluginProcessContainer> onDetermineDependentDocuments() {
@@ -44,41 +42,34 @@ public class CloneDependentDocumentService implements CloneDocumentsExtensionSer
   private PlmObjectKey findTheChosenOneDependentDocument(PlmObjectKey cloneDocumentKey) {
     PlmObjectKey depDocToClone = null;
     FmCallBuilder builder = new FmCallBuilder("BAPI_DOCUMENT_WHEREUSED");
-    Matcher m = PATTERN.matcher(cloneDocumentKey.getKey());
-    if (m.matches()) {
-      String type = m.group(1);
-      String number = m.group(2);
-      String version = m.group(3);
-      String part = m.group(4);
-      RfcCall rfcCall = builder.importing()
-          .scalar("DOCUMENTTYPE", type)
-          .scalar("DOCUMENTNUMBER", number)
-          .scalar("DOCUMENTVERSION", version)
-          .scalar("DOCUMENTPART", part).build();
-      RfcResult result = ectrService.getRfcExecutor().execute(rfcCall);
-      RfcStructure returnStructure = result.getExportParameter("RETURN").getStructure();
-      String retType = returnStructure.getFieldValue("TYPE");
-      if (ERROR_RET_TYPES.contains(retType))
-        ectrService.getPlmLogger().error("BAPI_DOCUMENT_WHEREUSED has returned an error, s. apilog, message: " + returnStructure.getFieldValue("MESSAGE"));
-      else {
-        var docStructureTable = result.getTable("DOCUMENTSTRUCTURE");
-        int rowCount = docStructureTable.getRowCount();
-        for (int i = 0; i < rowCount; i++) {
-          RfcStructure tableRow = docStructureTable.getRow(i);
-          String rDocType = tableRow.getFieldValue("DOCUMENTTYPE");
-          String rDocNumber = tableRow.getFieldValue("DOCUMENTNUMBER");
-          String rDocVersion = tableRow.getFieldValue("DOCUMENTVERSION");
-          String rDocPart = tableRow.getFieldValue("DOCUMENTPART");
-          String rSortString = tableRow.getFieldValue("SORTSTRING");
-          String rCadPos = tableRow.getFieldValue("CAD_POS");
-          if (type.equals(rDocType) && "X".equals(rCadPos) && !"D".equals(rSortString)) {
-            depDocToClone = new PlmObjectKey("DRAW", rDocType + rDocNumber + rDocPart + rDocVersion); // the sap key has to be build lie in sap!
-          }
+    DocumentKey documentKey = keyConverter.fromPlmObjectKey(cloneDocumentKey);
+
+    RfcCall rfcCall = builder.importing()
+        .scalar("DOCUMENTTYPE", documentKey.getType())
+        .scalar("DOCUMENTNUMBER", documentKey.getNumber())
+        .scalar("DOCUMENTVERSION", documentKey.getVersion())
+        .scalar("DOCUMENTPART", documentKey.getPart()).build();
+    RfcResult result = ectrService.getRfcExecutor().execute(rfcCall);
+    RfcStructure returnStructure = result.getExportParameter("RETURN").getStructure();
+    String retType = returnStructure.getFieldValue("TYPE");
+    if (ERROR_RET_TYPES.contains(retType))
+      ectrService.getPlmLogger().error("BAPI_DOCUMENT_WHEREUSED has returned an error, s. apilog, message: " + returnStructure.getFieldValue("MESSAGE"));
+    else {
+      var docStructureTable = result.getTable("DOCUMENTSTRUCTURE");
+      int rowCount = docStructureTable.getRowCount();
+      for (int i = 0; i < rowCount; i++) {
+        RfcStructure tableRow = docStructureTable.getRow(i);
+        String rDocType = tableRow.getFieldValue("DOCUMENTTYPE");
+        String rDocNumber = tableRow.getFieldValue("DOCUMENTNUMBER");
+        String rDocVersion = tableRow.getFieldValue("DOCUMENTVERSION");
+        String rDocPart = tableRow.getFieldValue("DOCUMENTPART");
+        String rSortString = tableRow.getFieldValue("SORTSTRING");
+        String rCadPos = tableRow.getFieldValue("CAD_POS");
+        if (documentKey.getType().equals(rDocType) && "X".equals(rCadPos) && !"D".equals(rSortString)) {
+          depDocToClone = keyConverter.plmObjectKeyForDocument(rDocType, rDocNumber, rDocVersion, rDocPart);
         }
       }
     }
     return depDocToClone;
   }
-
-
 }
